618f802b9b51a377d4f82217a59a8069ff2068be
Artem Popov <br4v0.2403@gmail.com>
2023-07-05 15:49:05 +0300

    set up configuration for limits

--- src/main/java/com/demo/microservices/limitsservice/bean/Limits.java
+++ src/main/java/com/demo/microservices/limitsservice/bean/Limits.java
@@ -1,2 +1,18 @@
-package com.demo.microservices.limitsservice.bean;public class Limits {
+package com.demo.microservices.limitsservice.bean;
+
+import com.demo.microservices.limitsservice.configuration.Configuration;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Data
+public class Limits {
+
+    private int minimum;
+    private int maximum;
+
+    public Limits(int minimum, int maximum) {
+        this.minimum = minimum;
+        this.maximum = maximum;
+    }
 }
--- src/main/java/com/demo/microservices/limitsservice/configuration/Configuration.java
+++ src/main/java/com/demo/microservices/limitsservice/configuration/Configuration.java
@@ -1,2 +1,30 @@
-package com.demo.microservices.limitsservice.configuration;public class Configuration {
+package com.demo.microservices.limitsservice.configuration;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+
+@Component
+@ConfigurationProperties("limits-service")
+public class Configuration {
+    private int minimum;
+    private int maximum;
+
+    public int getMinimum() {
+        return minimum;
+    }
+
+    public void setMinimum(int minimum) {
+        this.minimum = minimum;
+    }
+
+    public int getMaximum() {
+        return maximum;
+    }
+
+    public void setMaximum(int maximum) {
+        this.maximum = maximum;
+    }
+
 }
